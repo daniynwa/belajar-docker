bb0d7ff675c5a8b77fa8cc78c06e3f17dd745e53
Dani Afriyadi Yusuf <dani94andara@gmail.com>
2023-09-06 18:20:19 +0700

    Update docker-container.sh

--- docker-container.sh
+++ docker-container.sh
@@ -1,1 +1,15 @@
+"Membuat docker container"
 
+#Melihat semua container
+  docker container ls -a
+
+#Melihat container yang aktif / sedang berjalan
+  docker container ls
+
+#Membuat container
+contoh :  docker container create --name namacontainer namaimage:tag
+  docker container create --name xredis redis:latest
+
+#Menjalankan container
+contoh : docker container start containerid/namacontainer
+  docker container start xredis
