89f29556db7c09b2aec709476ea80caacf907c0c
Dani Afriyadi Yusuf <dani94andara@gmail.com>
2023-09-07 16:46:31 +0700

    Update docker-container-port.sh

--- docker-container-port.sh
+++ docker-container-port.sh
@@ -2,9 +2,10 @@
 
 contoh : docker container create --name namacontainer --publish posthost:portcontainer image:tag
   docker container create --name corpnginx --publish 8080:80 nginx:latest
-bash'''
+```bash
 [root@corp dani]# docker container ls -a
 CONTAINER ID   IMAGE          COMMAND                  CREATED          STATUS       PORTS      NAMES
 3f2ad9791abf   nginx:latest   "/docker-entrypoint.…"   18 seconds ago   Created                 corpnginx
 bc0dbb5776b2   redis:latest   "docker-entrypoint.s…"   4 hours ago      Up 4 hours   6379/tcp   credis
 [root@corp dani]#
+```
