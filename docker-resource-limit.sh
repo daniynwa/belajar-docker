4aa298a08099c05d55808fbd4a3891a0107671bb
Dani Afriyadi Yusuf <dani94andara@gmail.com>
2023-09-07 17:22:03 +0700

    Update docker-resource-limit.sh

--- docker-resource-limit.sh
+++ docker-resource-limit.sh
@@ -1,2 +1,6 @@
 "Cara melakukan pembatasan (limit) pada docker container"
 
+```bash
+contoh :  docker container create --name namacontainer --publish 8081:80 --memory 100m --cpus 0.5 namaimage:tag
+  docker container create --name smallnginx --publish 8081:80 --memory 100m --cpus 0.5 nginx:latest
+```
