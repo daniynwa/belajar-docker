97f5cfebb9f5e64d068b970ca1398c3e592b186c
Dani Afriyadi Yusuf <dani94andara@gmail.com>
2023-09-07 16:56:14 +0700

    Update docker-environment.sh

--- docker-environment.sh
+++ docker-environment.sh
@@ -2,5 +2,7 @@
 
 /biasa digunakan ketika kita menggunakan database "mongodb"
 
+```bash
 contoh : docker container create --name namacontainer --env KEY="value" image:tag
   docker container create --name corpmongodb --publish 27017:27017 --env MONGO_INITDB_ROOT_USERNAME=root --env MONGO_INITDB_ROOT_PASSWORD=mongo12345
+```
