198b7e7b1a0108a684ba31b2ad0bf9882bf30540
Dani Afriyadi Yusuf <dani94andara@gmail.com>
2023-09-07 10:33:08 +0700

    Update docker-container-logs.sh

--- docker-container-logs.sh
+++ docker-container-logs.sh
@@ -1,2 +1,9 @@
 "Managemen Logs docker container
 
+#Melihat logs docker container
+contoh : docker container logs containerid/namacontainer
+  docker container logs xredis
+
+#Melihat logs docker container dan menunggu logs baru
+contoh : docker container logs -f containerid/namacontainer
+  docker container logs -f xredis
