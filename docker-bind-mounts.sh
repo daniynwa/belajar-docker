0cc02d322f1010732a49a9bf323d7e731402e9ee
Dani Afriyadi Yusuf <dani94andara@gmail.com>
2023-09-08 10:49:34 +0700

    Update docker-bind-mounts.sh

--- docker-bind-mounts.sh
+++ docker-bind-mounts.sh
@@ -2,4 +2,4 @@
 
 
 contoh : docker container create --name namacontainer --mount "type=value,source=directory-source,destination=folder-directory-container" --publish 27018:27017 --env KEY="value" image:tag
-  docker container create --name corpmongodb --publish 27017:27017 --env MONGO_INITDB_ROOT_USERNAME=root --env MONGO_INITDB_ROOT_PASSWORD=mongo12345
+  docker container create --name daymongodb --mount "type=bind,source=/home/dani/mongo-data,destination=/data/db" --publish 27018:27017 --env MONGO_INITDB_ROOT_USERNAME=root --env MONGO_INITDB_ROOT_PASSWORD=mongo12345
