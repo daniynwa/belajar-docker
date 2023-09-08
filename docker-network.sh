322752b077ab034d146bc116477cf1b369a04369
Dani Afriyadi Yusuf <dani94andara@gmail.com>
2023-09-08 15:52:26 +0700

    Update docker-network.sh

--- docker-network.sh
+++ docker-network.sh
@@ -4,6 +4,12 @@ contoh :
 ```bash
 docker network ls /'untuk melihat list network'
 
-docker network create mongonetwork /'membuat network (mongonetwork)'
+docker network create --driver bridge mongonetwork /'membuat network (mongonetwork)'
 ```
+docker container create --name mongodb --network mongonetwork --env MONGO_INITDB_ROOT_USERNAME=root --env MONGO_INITDB_ROOT_PASSWORD=mongo12345 mongo:latest
 
+docker container start mongodb /'untuk menjalankan container mongodb'
+
+docker container create --name mongoexpress --network mongonetwork --publish 8081:8081 --env ME_CONFIG_MONGODB_URL="mongodb://root:mongo12345@mongodb:27017/" mongo-express:latest
+
+docker container start mongoexpress /'untuk menjalankan container mongoexpress'
