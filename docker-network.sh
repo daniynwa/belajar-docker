90a4b8b979a8da2360b626ccd67d328a89a8edfe
Dani Afriyadi Yusuf <dani94andara@gmail.com>
2023-09-08 15:56:24 +0700

    Update docker-network.sh

--- docker-network.sh
+++ docker-network.sh
@@ -13,3 +13,7 @@ docker container start mongodb /'untuk menjalankan container mongodb'
 docker container create --name mongoexpress --network mongonetwork --publish 8081:8081 --env ME_CONFIG_MONGODB_URL="mongodb://root:mongo12345@mongodb:27017/" mongo-express:latest
 
 docker container start mongoexpress /'untuk menjalankan container mongoexpress'
+
+docker network disconnect mongonetwork mongodb /'untuk menonaktifkan mongonetwork pada container (mongodb)'
+
+docker network connect mongonetwork mongodb /'untuk memasukan container (mongodb) ke dalam jaringan (mongonetwork)'
