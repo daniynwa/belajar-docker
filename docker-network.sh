70a83453c878a21941c56b83a006c9ea19309dd3
Dani Afriyadi Yusuf <dani94andara@gmail.com>
2023-09-08 15:29:59 +0700

    Create docker-network.sh

---
+++ docker-network.sh
@@ -0,0 +1,9 @@
+"Membuat docker network untuk antar container"
+
+contoh : 
+```bash
+docker network ls /'untuk melihat list network'
+
+docker network create mongonetwork /'membuat network (mongonetwork)'
+```
+
