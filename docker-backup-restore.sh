666bf10099494a80695c1d49d3908cf7a5b6ce77
Dani Afriyadi Yusuf <dani94andara@gmail.com>
2023-09-08 15:15:32 +0700

    Update docker-backup-restore.sh

--- docker-backup-restore.sh
+++ docker-backup-restore.sh
@@ -1,1 +1,5 @@
+"Cara Melakukan restore pada docker volume"
 
+contoh : docker volume create mongorestore
+
+docker container run --rm --name ubuntu --mount "type=bind,source=/home/dani/backup-volume,destination=/backup" --mount "type=volume,source=mongorestore,destination=/data" ubuntu:latest bash -c "cd /data && tar xvf /backup//mongovolume1.tar.gz --strip 1"
