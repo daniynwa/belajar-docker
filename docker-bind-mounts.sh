4a9ca72198863bcdc93541e2963ca9cc4846dff8
Dani Afriyadi Yusuf <dani94andara@gmail.com>
2023-09-08 10:40:07 +0700

    Update docker-bind-mounts.sh

--- docker-bind-mounts.sh
+++ docker-bind-mounts.sh
@@ -1,2 +1,5 @@
 "Docker bind mounts merupakan kemampuan untuk melakukan mounting (sharing) folder dalam container"
 
+
+contoh : docker container create --name namacontainer --mount "type=value,source=directory-source,destination=folder-directory-container" --publish 27018:27017 --env KEY="value" image:tag
+  docker container create --name corpmongodb --publish 27017:27017 --env MONGO_INITDB_ROOT_USERNAME=root --env MONGO_INITDB_ROOT_PASSWORD=mongo12345
